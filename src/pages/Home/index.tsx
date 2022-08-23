ce45c199941d83a5676c5022b018ece3874bed57
Matheus Chein <matheus.chein@fabric.inc>
2022-08-23 17:49:03 -0300

    creating home banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,52 @@
+import { ShoppingCart, Coffee, Timer, Package } from 'phosphor-react';
+
+import homeBannerImg from '../../assets/banner-cup.svg';
+
+import { BannerContainer, BannerItemsContainer, BannerItem } from './styles';
+
 export function Home() {
-  return <h1>home</h1>;
+  return (
+    <main>
+      <BannerContainer>
+        <aside>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <span>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </span>
+
+          <BannerItemsContainer>
+            <BannerItem iconColor="yellow700">
+              <div>
+                <ShoppingCart weight="fill" />
+              </div>
+              <span>Compra simples e segura</span>
+            </BannerItem>
+
+            <BannerItem iconColor="baseText">
+              <div>
+                <Package weight="fill" />
+              </div>
+              <span>Embalagem mantém o café intacto</span>
+            </BannerItem>
+
+            <BannerItem iconColor="yellow500">
+              <div>
+                <Timer weight="fill" />
+              </div>
+              <span>Entrega rápida e rastreada</span>
+            </BannerItem>
+
+            <BannerItem iconColor="purple500">
+              <div>
+                <Coffee weight="fill" />
+              </div>
+              <span>O café chega fresquinho até você</span>
+            </BannerItem>
+          </BannerItemsContainer>
+        </aside>
+        <img src={homeBannerImg} alt="Coffee cup" />
+      </BannerContainer>
+    </main>
+  );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const BannerContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 544px;
+
+  &:before {
+    content: '';
+    position: fixed;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    background-image: url('src/assets/banner-background.svg');
+    width: 100%;
+    height: 544px;
+  }
+
+  aside {
+    max-width: 50%;
+
+    h1 {
+      font-size: 3rem;
+      font-weight: 800;
+      font-family: 'Baloo 2', sans-serif;
+      color: ${props => props.theme.baseTitle};
+      line-height: 1.3;
+      margin-bottom: 1rem;
+    }
+
+    > span {
+      color: ${props => props.theme.baseSubtitle};
+      font-size: 1.25rem;
+      line-height: 26px;
+      display: inline-block;
+      margin-bottom: 4.125rem;
+    }
+  }
+`;
+
+export const BannerItemsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2.5rem;
+`;
+
+enum iconColors {
+  yellow700,
+  baseText,
+  purple500,
+  yellow500,
+}
+
+interface BannerItemProps {
+  iconColor: keyof typeof iconColors;
+}
+
+export const BannerItem = styled.div<BannerItemProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  max-width: 225px;
+
+  div {
+    background: ${props => props.theme[props.iconColor]};
+    border-radius: 50%;
+    padding: 8px;
+    display: flex;
+    align-items: center;
+
+    svg {
+      color: ${props => props.theme.background};
+    }
+  }
+  span {
+    line-height: 1.3;
+  }
+`;
